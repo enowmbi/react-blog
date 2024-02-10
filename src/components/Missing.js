eabd4a21f9038f8059ea07ebb6fe703f0b2d8e70
Enow Mbi <benowmbi@yahoo.com>
2024-02-10 20:55:42 +0100

    Add more content to missing component

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const Missing = () =>{
     return(
         <main>
-            <h2> Missing </h2>
+            <h2>Page not found!</h2>
+            <p>Well, that's disappointing</p>
+            <Link to="/">visit our home page</Link>
          </main>
     )
 
